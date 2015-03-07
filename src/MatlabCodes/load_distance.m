93396381077f4a4cc602e880bf367e72efc46bca
yuconglin <astroinstrulin@gmail.com>
2015-03-07 16:34:47 -0700

    tried to make the old version work but it didn't works

--- src/MatlabCodes/load_distance.m
+++ src/MatlabCodes/load_distance.m
@@ -2,27 +2,35 @@ close all;
 clear all;
 clc;
 
-A=load('../../records/divide_record.txt');
+A=load('../../records/random3.txt');
 dis_v=[];
 dis_h=[];
 
 [m,n]=size(A);
+% for i=1:m
+%   if mod(i,2) == 1
+%      dis_v = [dis_v,A(i,:)];
+%   else
+%      dis_h = [dis_h,A(i,:)];
+%   end
+% end
 for i=1:m
-  if mod(i,2) == 1
-     dis_v = [dis_v,A(i,:)];
-  else
-     dis_h = [dis_h,A(i,:)];
-  end
+    dis_v = [dis_v,A(i,2)];
+    dis_h = [dis_h,A(i,1)];
 end
 
-vv= 300;
-hh= 50;
+hh= 300;
+vv= 50;
 
 figure;
-scatter(dis_v,dis_h);
+scatter(dis_h,dis_v);
 hold on;
+grid on;
+axis equal;
 
 rectangle('Position',[0,0,300,50]);
+xlabel('horizontal(m)');
+ylabel('vertical(m)');
 
 
 
