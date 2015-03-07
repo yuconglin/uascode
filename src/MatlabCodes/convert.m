0de0e47dd43ea9c654d68fb1aa85df214f231dd0
yuconglin <astroinstrulin@gmail.com>
2015-03-07 15:50:09 -0700

    commit before switch branches

--- src/MatlabCodes/convert.m
+++ src/MatlabCodes/convert.m
@@ -1,9 +1,9 @@
 close all;
-open rs_ls.fig;
+open random3.fig;
 %grid on;
 
 h = findobj(gca, 'Type', 'line');
 set(h,'MarkerSize',5)
 set(gca,'Color','none');
 %set(gcf, 'Color', 'w') 
-export_fig('rs_ls', '-pdf', '-transparent' );
+export_fig('random3', '-pdf', '-transparent' );
