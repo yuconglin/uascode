d351c77f4514df6efdd476abcf37984b32e0692a
yuconglin <astroinstrulin@gmail.com>
2015-04-01 23:01:45 -0700

    updated the test_MavrosListen.sh

--- test_MavrosListen.sh
+++ test_MavrosListen.sh
@@ -5,7 +5,7 @@ gnome-terminal \
 	--tab --title "mavros" --command "bash -c \"
 				env sleep 3s;
                                 #rosrun mavros mavros_node _fcu_url:=/dev/ttyUSB0  _gcs_url:='udp://@localhost' _mission/pull_after_gcs:='true'
-				roslaunch mavros px4.launch fcu_url:="/dev/ttyUSB0:57600"  gcs_url:="udp://@localhost" mission/pull_after_gcs:='true'
+				roslaunch mavros px4.launch fcu_url:="/dev/ttyUSB0:57600"  gcs_url:="udp://:14555@10.206.164.162:14550" mission/pull_after_gcs:="true"
 				exec bash\"" \
 	--tab --title "MavrosListen" --command "bash -c \"
 				env sleep 3s;
